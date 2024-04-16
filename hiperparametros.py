260ff8e0a901c9d4c43b3c38fd39ff819b566ff0
Luiscxz <lionsgateazul@gmail.com>
2024-04-16 12:22:00 -0500

    Se modifica la función #ver_resultados# y se agrega comentarios del funcionamiento

--- hiperparametros.py
+++ hiperparametros.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 import time
 from plotnine import *
+
 # estableciendo ruta donde estan los archivos
 ruta = 'E:\\4. repositorios github\\ML_Py_23\\data'
 os.chdir(ruta)
@@ -19,51 +20,57 @@ file = pd.read_csv('datos_iris.csv')
 # seleccionando columnas con datos tipo float
 var_independientes = file.select_dtypes(['float'])
 var_objetivo = file.select_dtypes(['object'])
-#%% Importando libreria de validación cruzada
-from sklearn.model_selection import cross_validate
-# creando funcion que hace validación cruzada para 5 particiones (cv=5) y
-# que lo haga para el score=f1
-def evaluar_modelo(estimador,X,Y):
-    resultados_estimador = cross_validate(estimator, X, Y,
-                                          scoring='f1_micro',# se usa por que las clases estan valanceada,
-                                          # eso quiere decir que hay 50 flores de una clase y 50 de otra
-                                          cv =5,
-                                          n_jobs=-1)
-# procedemos a crear la función para ver los resultados
-def ver_resultados():
-    # creando dataframe de pandas llamado resultado y lo trasponemos
-    resultados_df = pd.DataFrame(resultados).T
-    # obteniendo los nombres de las columnas del dataframe
-    resultados_cols = resultados_df.colums
-    # procedemos a iterar sobre cada columna del dataframe
-    for col in resultados_df:
-        # procedemos a calcular el valor maximo de la columna
-        maximo = resultados_df[col].max()
-        #calculando promedio de la columna y reemplazando todas las filas con el promedio clculado
-        resultados_df[col]=resultados_df[col].apply(np.mean)
-        # creando nueva columna en el dataframe 
-        # procedemos a dividir cada valor de calumna por el valor maximo 
-        resultados_df[col+"_idx"] = resultados_df[col]/maximo 
-    resultados_df = resultados_df.sort_values(by=["test_score", "fit_time"], ascending=[False, True])
-    return resultados_df
 #%%
-# importando librerias de los modelos de aprendizaje supervisado
+from sklearn.model_selection import cross_validate
+# creando funcion que evalua los modelos
+def evaluar_modelo(estimador, independientes, objetivo):
+    # realizando validación cruzada
+    resultados_estimador = cross_validate(estimador, independientes, objetivo,
+                     scoring="f1_micro", n_jobs=-1, cv=5)
+    return resultados_estimador
+#%%
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import tree
 from sklearn.svm import SVC
-# procedemos a crear un diccionario donde guardaremos los cinco puntuaciones f1
-#correspondientes a las 5 diviciones que definí en la función evaluar modelo 
+# procedemos a llamar la funcion evaluar modelo y guardamos los resultados
+# en un diccionario
 resultados = {}
-# procedemos a evaluar los modelos y guardar los 5 resultados en el diccionaio
+
 resultados["knn"] = evaluar_modelo(KNeighborsClassifier(),
                                    var_independientes,
                                    var_objetivo['Species'])
 resultados["arbol_clasificacion"] = evaluar_modelo(tree.DecisionTreeClassifier(),
-                                   var_independientes,
-                                   var_objetivo['Species'])
+                                    var_independientes,
+                                    var_objetivo['Species'])
 resultados["arbol_aleatorio"] = evaluar_modelo(tree.ExtraTreeClassifier(),
-                                   var_independientes,
-                                   var_objetivo['Species'])
+                                    var_independientes,
+                                    var_objetivo['Species'])
 resultados["msv"] = evaluar_modelo(SVC(),
                                    var_independientes,
                                    var_objetivo['Species'])
+
+#%% Procedemos a crear la funcion para ver los resultados
+def ver_resultados(resultados):
+    # procedemos a convertir el diccionario a un dataframe
+    resultados_df = pd.DataFrame(resultados).T
+    # obteniendo nombre de las columnas
+    resultados_col = resultados_df.columns
+    # procedemos recorrer las columnas mediante ciclo for
+    for col in resultados_df:
+        """ Dado que cada fila de cada columna contiene la información en 
+        el siguiente formato:[0.00399971 0.00400662 0.00399971 0.00399923 0.00400662]
+        se procede a cálcular el promedio de estos valores en la fila y todos estos valores
+        se reemplazan por un unico valor (el valor promedio)
+        """
+        resultados_df[col] = resultados_df[col].apply(np.mean)
+        """ a cada columna se le consulta el valor máximo, y cada fila de esa
+        columna, se divide por el valor maximo. Esto se hace para normalizar los
+        datos de las columnas
+        """
+        resultados_df[col+"_idx"] = resultados_df[col] / resultados_df[col].max()       
+    return resultados_df
+# procedemos a ver los resultados
+resultados_df= ver_resultados(resultados)
+# organizando resultados y mostrando en pantalla.
+resultados_df = resultados_df.sort_values(by=['test_score', 'fit_time'], ascending=[False, True])
+print(resultados_df)
