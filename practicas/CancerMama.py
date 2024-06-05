b1001cd0bd3a9eefa215c0cdab9ccf3715ba20b9
Luiscxz <lionsgateazul@gmail.com>
2024-06-05 11:16:36 -0500

    Se agrega hiperparámetros

--- practicas/CancerMama.py
+++ practicas/CancerMama.py
@@ -79,13 +79,7 @@ boxplotChl = independientes.boxplot(column= columnas[2:31],
 boxplotChl.set_title('Datos normalizados', fontsize=20, fontweight='bold')
 # Procedemos a codificar las variables Categoricas de forma manual
 objetivo = objetivo.replace({'M':1,'B':0})
-#%%  Hyper-parámetros
-# Importando librerías de modelos a usar
-from sklearn.model_selection import cross_validate
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import tree
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
+#%%  Definimos los hyperparatros
 """
 Usaré los modelos: 
     KNN
@@ -94,8 +88,168 @@ Usaré los modelos:
     Regresión Logística
     
 """
+# Estableciendo parámetros de busqueda para knn
+parametros_knn = {
+    "n_neighbors": [1,10,20,30,40,50],
+    "p": [1,2,3],
+    "weights": ["uniform", "distance"]
+}
+# estableciendo parámetros de busqueda para SVM
+parametros_svm_poly = {
+    "degree": [1,2,3,4],
+    "kernel": ["poly"]
+}
+
+parametros_svm_gauss = {
+    "gamma": [0.1,0.5,1.,10.],
+    "kernel": ["rbf"]
+}
+ 
+# Estableciendo parámetros para arbol 
+parametros_arbol = {
+    "max_depth": list(range(3,6)),
+    "criterion": ["gini","entropy"],    
+    "class_weight": [None,"balanced"]
+    }
+
+parametros_busqueda_svm = {
+    "degree": [1,2,3,4],
+    "gamma": [0.1,0.5,1.,10.],
+    "kernel": ["poly", "rbf"]}
+
+""" Nota: El arbol aleatorio no se le hace optimización de hiperparámetros
+"""
+#%% Escribimos las funciones de evalución de los modelos y visualización de resultados
+# Importando librerías de modelos a usar
+from sklearn.model_selection import cross_validate
 # Creando función que evalua los modelos mediante validación cruzada
 def evaluar_modelo(estimador, independientes, objetivo):
+    """
+    Parameters
+    ----------
+    estimador : Modelo de aprendizaje supervisado
+    independientes : Dataframe
+        Es el dataframe de variables independientes .
+    objetivo : Serie
+        Contiene las etiquetas del set de datos.
+
+    Returns
+    -------
+    resultados_estimador : Puntuación F1 Score
+        Devuelve las metricas calculadas.
+    """
     resultados_estimador = cross_validate(estimador, independientes, objetivo,
                      scoring="f1_micro", n_jobs=-1, cv=5)
     return resultados_estimador
+# definiendo función que muestra los resultados 
+def ver_resultados(resultados):
+    # procedemos a convertir el diccionario a un dataframe
+    resultados_df = pd.DataFrame(resultados).T
+    # obteniendo nombre de las columnas
+    resultados_col = resultados_df.columns
+    # procedemos recorrer las columnas mediante ciclo for
+    for col in resultados_df:
+        """ Dado que cada fila de cada columna contiene la información en 
+        el siguiente formato:[0.00399971 0.00400662 0.00399971 0.00399923 0.00400662]
+        se procede a cálcular el promedio de estos valores en la fila y todos estos valores
+        se reemplazan por un unico valor (el valor promedio)
+        """
+        resultados_df[col] = resultados_df[col].apply(np.mean)
+        """ a cada columna se le consulta el valor máximo, y cada fila de esa
+        columna, se divide por el valor maximo. Esto se hace para normalizar los
+        datos de las columnas
+        """
+        resultados_df[col+"_idx"] = resultados_df[col] / resultados_df[col].max()       
+    return resultados_df
+#%% Creación de modelos a usar
+from sklearn.model_selection import RandomizedSearchCV,GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import tree
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+# Definiendo modelos 
+estimador_knn = KNeighborsClassifier()
+estimador_svm = SVC()
+estimador_arbol = tree.DecisionTreeClassifier()
+estimador_arbol_aleatorio = tree.ExtraTreeClassifier()
+""" Creando modelos para busqueda aleatoria, que encuentra la mejor combinación
+de estos parámetros. Internamente RandomizedSearchCV divide los datos en múltiples
+pliegues de entrenamiento y validación. Genera combinaciones aleatorias, y para 
+cada combinación, entrena el modelo.
+"""
+knn_Random = RandomizedSearchCV(estimator=estimador_knn, 
+                    param_distributions=parametros_knn,
+                    scoring="f1_micro", n_jobs=-1)
+
+svm_poly_Random = RandomizedSearchCV(estimator=estimador_svm, 
+                    param_distributions=parametros_svm_poly,
+                    scoring="f1_micro", n_jobs=-1)
+
+svm_gauss_Random = RandomizedSearchCV(estimator=estimador_svm, 
+                    param_distributions=parametros_svm_gauss,
+                    scoring="f1_micro", n_jobs=-1)
+
+arbol_Random = RandomizedSearchCV(estimator=estimador_arbol, 
+                    param_distributions=parametros_arbol,
+                    scoring="roc_auc", n_jobs=-1)
+#%% Definiendo modelos para busqueda por malla
+knn_grid = GridSearchCV(estimator=estimador_knn, 
+                    param_grid=parametros_knn,
+                    scoring="f1_micro", n_jobs=-1)
+
+svm_grid = GridSearchCV(estimator=estimador_svm, 
+                    param_grid=parametros_busqueda_svm,
+                    scoring="f1_micro", n_jobs=-1)
+
+arbol_grid = GridSearchCV(estimator=estimador_arbol, 
+                    param_grid=parametros_arbol,
+                    scoring="roc_auc", n_jobs=-1)
+#%% Ajustando los modelos
+"""
+Se toma el conjunto de datos (independientes y objetivo), se realiza la busqueda
+aleatoria de hiperparámetros, y se entrenan y evaluan los modelos con cada combinación
+de hyperparámetros y se selecciona la mejor combinación de hiperparámetros basad
+ en el F1 score micro
+"""
+# Ajustando modelos por busqueda aleatoria
+knn_Random.fit(independientes, objetivo)
+svm_poly_Random.fit(independientes, objetivo)
+svm_gauss_Random.fit(independientes, objetivo)
+arbol_Random.fit(independientes, objetivo)
+# ajustando modelos por busqueda en malla
+knn_grid.fit(independientes, objetivo)
+svm_grid.fit(independientes, objetivo)
+arbol_grid.fit(independientes, objetivo)
+#%% Obtención de resultados apartir del mejor estimador
+
+resultados = {}
+# sección busqueda por malla
+resultados["knn_grid"] = evaluar_modelo(knn_grid.best_estimator_,
+                                   independientes,
+                                   objetivo)
+resultados["svm_grid"] = evaluar_modelo(svm_grid.best_estimator_,
+                                   independientes,
+                                   objetivo)
+resultados["arbol_grid"] = evaluar_modelo(arbol_grid.best_estimator_,
+                                   independientes,
+                                   objetivo)
+# sección busqueda aleatoria
+resultados["knn_ramdon"] = evaluar_modelo(knn_Random.best_estimator_,
+                                   independientes,
+                                   objetivo)
+resultados["svm_poly_Random"] = evaluar_modelo(svm_poly_Random.best_estimator_,
+                                   independientes,
+                                   objetivo)
+resultados["svm_gauss_Random"] = evaluar_modelo(svm_gauss_Random.best_estimator_,
+                                   independientes,
+                                   objetivo)
+resultados["arbol_Random"] = evaluar_modelo(arbol_Random.best_estimator_,
+                                   independientes,
+                                   objetivo)
+resultados["arbol_aleatorio"] = evaluar_modelo(estimador_arbol_aleatorio,
+                                               independientes,
+                                               objetivo)
+# observando los resultados
+resultados_df= ver_resultados(resultados)
+# organizando resultados.
+resultados_df = resultados_df.sort_values(by=['test_score', 'fit_time'], ascending=[False, True])
