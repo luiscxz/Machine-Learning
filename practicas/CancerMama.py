29df664778170bc90d9ccce4140e60ed23db5765
Luiscxz <lionsgateazul@gmail.com>
2024-07-04 19:13:00 -0500

    se continua en la mejora del modelo

--- practicas/CancerMama.py
+++ practicas/CancerMama.py
@@ -8,6 +8,7 @@ Algoritmo que usa diferentes modelos para detectar cancer de mama
 import os 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 # Accediendo a donde están los datos
 os.chdir('D:\\3. Cursos\\9. Data machine learning\\CancerDeMama')
 # Leyendo archivo csv
@@ -117,7 +118,7 @@ parametros_busqueda_svm = {
     "gamma": [0.1,0.5,1.,10.],
     "kernel": ["poly", "rbf"]}
 
-""" Nota: El arbol aleatorio no se le hace optimización de hiperparámetros
+""" Nota: El árbol aleatorio no se le hace optimización de hiperparámetros
 """
 #%% Escribimos las funciones de evalución de los modelos y visualización de resultados
 # Importando librerías de modelos a usar
@@ -127,7 +128,7 @@ def evaluar_modelo(estimador, independientes, objetivo):
     """
     Parameters
     ----------
-    estimador : Modelo de aprendizaje supervisado
+    estimador : Modelo de aprendizaje supervisado con los mejores parámetros
     independientes : Dataframe
         Es el dataframe de variables independientes .
     objetivo : Serie
@@ -139,28 +140,9 @@ def evaluar_modelo(estimador, independientes, objetivo):
         Devuelve las metricas calculadas.
     """
     resultados_estimador = cross_validate(estimador, independientes, objetivo,
-                     scoring="f1_micro", n_jobs=-1, cv=5)
+                     scoring="f1_macro", n_jobs=-1, cv=10)
     return resultados_estimador
-# definiendo función que muestra los resultados 
-def ver_resultados(resultados):
-    # procedemos a convertir el diccionario a un dataframe
-    resultados_df = pd.DataFrame(resultados).T
-    # obteniendo nombre de las columnas
-    resultados_col = resultados_df.columns
-    # procedemos recorrer las columnas mediante ciclo for
-    for col in resultados_df:
-        """ Dado que cada fila de cada columna contiene la información en 
-        el siguiente formato:[0.00399971 0.00400662 0.00399971 0.00399923 0.00400662]
-        se procede a cálcular el promedio de estos valores en la fila y todos estos valores
-        se reemplazan por un unico valor (el valor promedio)
-        """
-        resultados_df[col] = resultados_df[col].apply(np.mean)
-        """ a cada columna se le consulta el valor máximo, y cada fila de esa
-        columna, se divide por el valor maximo. Esto se hace para normalizar los
-        datos de las columnas
-        """
-        resultados_df[col+"_idx"] = resultados_df[col] / resultados_df[col].max()       
-    return resultados_df
+
 #%% Creación de modelos a usar
 from sklearn.model_selection import RandomizedSearchCV,GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
@@ -179,31 +161,31 @@ cada combinación, entrena el modelo.
 """
 knn_Random = RandomizedSearchCV(estimator=estimador_knn, 
                     param_distributions=parametros_knn,
-                    scoring="f1_micro", n_jobs=-1)
+                    scoring="f1_macro", n_jobs=-1,n_iter=4)
 
 svm_poly_Random = RandomizedSearchCV(estimator=estimador_svm, 
                     param_distributions=parametros_svm_poly,
-                    scoring="f1_micro", n_jobs=-1)
+                    scoring="f1_macro", n_jobs=-1,n_iter=4)
 
 svm_gauss_Random = RandomizedSearchCV(estimator=estimador_svm, 
                     param_distributions=parametros_svm_gauss,
-                    scoring="f1_micro", n_jobs=-1)
+                    scoring="f1_macro", n_jobs=-1,n_iter=4)
 
 arbol_Random = RandomizedSearchCV(estimator=estimador_arbol, 
                     param_distributions=parametros_arbol,
-                    scoring="roc_auc", n_jobs=-1)
+                    scoring="f1_macro", n_jobs=-1,n_iter=4)
 #%% Definiendo modelos para busqueda por malla
 knn_grid = GridSearchCV(estimator=estimador_knn, 
                     param_grid=parametros_knn,
-                    scoring="f1_micro", n_jobs=-1)
+                    scoring="f1_macro", n_jobs=-1)
 
 svm_grid = GridSearchCV(estimator=estimador_svm, 
                     param_grid=parametros_busqueda_svm,
-                    scoring="f1_micro", n_jobs=-1)
+                    scoring="f1_macro", n_jobs=-1)
 
 arbol_grid = GridSearchCV(estimator=estimador_arbol, 
                     param_grid=parametros_arbol,
-                    scoring="roc_auc", n_jobs=-1)
+                    scoring="f1_macro", n_jobs=-1)
 #%% Ajustando los modelos
 """
 Se toma el conjunto de datos (independientes y objetivo), se realiza la busqueda
@@ -220,7 +202,8 @@ arbol_Random.fit(independientes, objetivo)
 knn_grid.fit(independientes, objetivo)
 svm_grid.fit(independientes, objetivo)
 arbol_grid.fit(independientes, objetivo)
-#%% Obtención de resultados apartir del mejor estimador
+
+#%% Obtención de resultados apartir del mejor estimador y haciendo validación cruzada
 
 resultados = {}
 # sección busqueda por malla
@@ -249,7 +232,40 @@ resultados["arbol_Random"] = evaluar_modelo(arbol_Random.best_estimator_,
 resultados["arbol_aleatorio"] = evaluar_modelo(estimador_arbol_aleatorio,
                                                independientes,
                                                objetivo)
-# observando los resultados
+#%%# definiendo función que muestra los resultados 
+def ver_resultados(resultados):
+    # procedemos a convertir el diccionario a un dataframe
+    resultados_df = pd.DataFrame(resultados).T
+    # obteniendo nombre de las columnas
+    resultados_col = resultados_df.columns
+    # procedemos recorrer las columnas mediante ciclo for
+    for col in resultados_df:# Col toma los nombres de las columnas
+        """ Dado que cada fila de cada columna contiene la información en 
+        el siguiente formato:[0.00399971 0.00400662 0.00399971 0.00399923 0.00400662]
+        se procede a cálcular el promedio de estos valores en la fila y todos estos valores
+        se reemplazan por un unico valor (el valor promedio)
+        """
+        resultados_df[col] = resultados_df[col].apply(np.mean)
+        """ a cada columna se le consulta el valor máximo, y cada fila de esa
+        columna, se divide por el valor maximo. Esto se hace para normalizar los
+        datos de las columnas
+        """
+        resultados_df[col+"_idx"] = resultados_df[col] / resultados_df[col].max()       
+    return resultados_df
+#%% observando los resultados
 resultados_df= ver_resultados(resultados)
 # organizando resultados.
 resultados_df = resultados_df.sort_values(by=['test_score', 'fit_time'], ascending=[False, True])
+""" una vez observado e identificado el modelo con mejor puntuación f1_score,
+procedemos a identificar sus mejores parámetros
+"""
+svm_grid.best_params_
+#%% Volvemos a crear el modelo y esta vez lo corremos con los mejores parámetros
+mejores_params = {'degree': 2, 'gamma': 1.0, 'kernel': 'poly'}
+mejor_svm = SVC(**mejores_params,probability=True)
+# entrenando el modelo
+mejor_svm.fit(independientes, objetivo)
+""" Continuar con la llegada de datos nuevos
+"""
+#%% Procedemos a ver como influyen las características en las decisiones del modelo
+import shap
